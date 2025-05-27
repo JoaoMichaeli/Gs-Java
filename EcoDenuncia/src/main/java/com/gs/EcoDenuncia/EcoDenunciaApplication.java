d9e44b548688718835855bf5c419f4d3ce505606
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-05-27 11:52:12 -0300

    init

---
+++ EcoDenuncia/src/main/java/com/gs/EcoDenuncia/EcoDenunciaApplication.java
@@ -0,0 +1,13 @@
+package com.gs.EcoDenuncia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoDenunciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoDenunciaApplication.class, args);
+	}
+
+}
---
+++ EcoDenuncia/src/test/java/com/gs/EcoDenuncia/EcoDenunciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.EcoDenuncia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDenunciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
